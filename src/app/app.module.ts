8feb5a2ba9c21e3fab618a4e9edef7a3913eb291
Mayara <mayaracardinot1999@gmail.com>
2025-07-27 18:46:44 -0300

    fix(dashboard): Ajuste do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { LoginComponent } from './auth/login/login.component';
+import { CardsComponent } from './dashboard/cards/cards.component';
+import { ChartComponent } from './dashboard/chart/chart.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
-    AppComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LoginComponent,
+    DashboardComponent,
+    ChartComponent,
+    CardsComponent,
+    AppComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
